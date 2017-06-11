92d79b605e7a751e14d27147a85e18e134b20234
Carlos R Arrivillaga <carlosra210@gmail.com>
2017-06-11 11:56:08 -0400

    testing

--- app/src/main/java/com/example/carlos/myapplication/actions/EliminarActivity.java
+++ app/src/main/java/com/example/carlos/myapplication/actions/EliminarActivity.java
@@ -2,6 +2,7 @@ package com.example.carlos.myapplication.actions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.carlos.myapplication.R;
 
@@ -11,5 +12,13 @@ public class EliminarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eliminar);
+        showToolbar(getResources().getString(R.string.elim_bar), true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
--- app/src/main/java/com/example/carlos/myapplication/actions/ModificarActivity.java
+++ app/src/main/java/com/example/carlos/myapplication/actions/ModificarActivity.java
@@ -2,6 +2,7 @@ package com.example.carlos.myapplication.actions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.carlos.myapplication.R;
 
@@ -11,5 +12,13 @@ public class ModificarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar);
+        showToolbar(getResources().getString(R.string.modif_bar), true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
