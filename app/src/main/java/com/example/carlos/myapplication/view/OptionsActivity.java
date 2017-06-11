387dbd53586b719494692093b659b66f8347c94b
Carlos R Arrivillaga <carlosra210@gmail.com>
2017-06-11 01:27:21 -0400

    creacion de menu

---
+++ app/src/main/java/com/example/carlos/myapplication/view/OptionsActivity.java
@@ -0,0 +1,16 @@
+package com.example.carlos.myapplication.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import com.example.carlos.myapplication.R;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
